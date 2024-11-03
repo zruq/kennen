559078312e618c5ba7c868becbdcb7c3952b354e
zruq <zruq@proton.me>
2024-11-03 19:53:00 +0100

    Fix build error

--- src/app/(auth)/collections/[collectionId]/new-question/page.tsx
+++ src/app/(auth)/collections/[collectionId]/new-question/page.tsx
@@ -1,12 +1,14 @@
-import { type NextPage } from "next";
 import NewQuestion from "./_components/new-question";
 import { api } from "@/trpc/server";
 
-const NewQuestionPage: NextPage<{ params: { collectionId: string } }> = async (
-  props,
-) => {
+const NewQuestionPage = async ({
+  params,
+}: {
+  params: Promise<{ collectionId: string }>;
+}) => {
+  const id = Number((await params).collectionId);
   const collection = await api.collection.byId({
-    id: Number(props.params.collectionId),
+    id,
   });
   return <NewQuestion collectionId={collection.id} />;
 };
