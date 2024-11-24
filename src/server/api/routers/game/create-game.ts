daa3a2dd7d14a1632eeef92212ff644fdae2623e
zruq <zruq@proton.me>
2024-11-24 14:14:20 +0100

    Randomize questions for each game

--- src/server/api/routers/game/create-game.ts
+++ src/server/api/routers/game/create-game.ts
@@ -2,8 +2,26 @@ import { env } from "@/env";
 import { publicProcedure } from "@/server/api/trpc";
 import type { PrismaClient } from "@prisma/client";
 import { z } from "zod";
+import { db } from "@/server/db";
 
-async function getQuestions(db: PrismaClient) {
+async function getQuestionsIds(collections: Array<number>) {
+  return db.question.findMany({
+    select: { id: true },
+    where: { collectionId: { in: collections } },
+  });
+}
+
+async function getQuestions(
+  db: PrismaClient,
+  {
+    collections,
+    questionsCount,
+  }: { collections: Array<number>; questionsCount: number },
+) {
+  const ids = getRandomSubarray(
+    await getQuestionsIds(collections),
+    questionsCount,
+  ).map(({ id }) => id);
   const questions = await db.question.findMany({
     select: {
       id: true,
@@ -21,13 +39,19 @@ async function getQuestions(db: PrismaClient) {
         },
       },
     },
+    where: {
+      id: { in: ids },
+    },
   });
-  return questions.map(({ text, multipleChoice, id }) => ({
-    id,
-    text,
-    options: multipleChoice?.options ?? [],
-    choicesLimit: multipleChoice?.choices_limit ?? 1,
-  }));
+  return ids.map((id) => {
+    const { text, multipleChoice } = questions.find((q) => q.id === id)!;
+    return {
+      id,
+      text,
+      options: multipleChoice?.options ?? [],
+      choicesLimit: multipleChoice?.choices_limit ?? 1,
+    };
+  });
 }
 
 export type Quiz = Awaited<ReturnType<typeof getQuestions>>;
@@ -37,7 +61,10 @@ export const create = publicProcedure
   .mutation(async ({ ctx }) => {
     const gameId = crypto.randomUUID();
     const userId = ctx.session?.user?.id ?? crypto.randomUUID();
-    const questions = await getQuestions(ctx.db);
+    const questions = await getQuestions(ctx.db, {
+      questionsCount: 4,
+      collections: [4, 5, 6, 7],
+    });
     await fetch(env.NEXT_PUBLIC_PARTYKIT_URL + `/party/${gameId}`, {
       method: "POST",
       headers: {
@@ -48,3 +75,17 @@ export const create = publicProcedure
     });
     return { gameId, userId };
   });
+
+function getRandomSubarray<T>(arr: T[], length: number): T[] {
+  if (length > arr.length) {
+    length = arr.length;
+  }
+  const shuffled = [...arr];
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j]!, shuffled[i]!];
+  }
+
+  return shuffled.slice(0, length);
+}
